b391fe1a9f8f1ebb0e79fc9df15a46ffde20c8f1
Derek Chai <chderek9@gmail.com>
2025-07-24 09:52:30 +1200

    Add option parsing

---
+++ UoATimetableSplitter/TimetableSplitterOptions.swift
@@ -0,0 +1,20 @@
+//
+//  TimetableSplitterOptions.swift
+//  UoATimetableSplitter
+//
+//  Created by Derek Chai on 24/07/2025.
+//
+
+import Foundation
+import ArgumentParser
+
+@main
+struct TimetableSplitterOptions: ParsableCommand {
+    /// The path to the .ics file to be split.
+    @Argument(help: "The path to the .ics file to be split.")
+    var path: String
+    
+    mutating func run() throws {
+        print("Path: \(path)")
+    }
+}
