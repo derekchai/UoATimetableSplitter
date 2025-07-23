933d2f4756e7bf020f85d3f234b7bd403fb82131
Derek Chai <chderek9@gmail.com>
2025-07-24 09:48:23 +1200

    Add package

---
+++ UoATimetableSplitter/TimetableSplitter.swift
@@ -0,0 +1,16 @@
+//
+//  TimetableSplitter.swift
+//  UoATimetableSplitter
+//
+//  Created by Derek Chai on 24/07/2025.
+//
+
+import Foundation
+import ArgumentParser
+
+@main
+struct TimetableSplitter: ParsableCommand {
+    mutating func run() throws {
+        print("Hello World")
+    }
+}
